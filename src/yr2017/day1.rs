e08924947b5327261b933877658962fc54c79592
James Cowgill <jcowgill@jcowgill.uk>
2017-12-02 18:35:26 +0000

    2017-01-2: implement

--- src/yr2017/day1.rs
+++ src/yr2017/day1.rs
@@ -19,6 +19,23 @@ pub fn star1(input: &str) -> String {
     sum.to_string()
 }
 
+/// Sum of all digits that match the digit halfway around the list
 pub fn star2(input: &str) -> String {
-    return String::from("Hello Worldz");
+    // Get a duplicated character list (string appended to itself)
+    let mut chars: Vec<char> = input.chars().collect();
+    let mut chars_cloned = chars.clone();
+    chars.append(&mut chars_cloned);
+
+    let mut sum = 0;
+    for (i, c) in chars.iter().enumerate() {
+        // Exit if we get to the end of the string
+        if i >= chars.len() / 2 { break }
+
+        // Add to sum if the current character matches the one halfway around
+        if *c == chars[i + chars.len() / 4] {
+            sum += (*c).to_digit(10).unwrap();
+        }
+    }
+
+    sum.to_string()
 }
