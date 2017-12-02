b65d3a700484f349763fbaae21f61756d2f2a25c
James Cowgill <jcowgill@jcowgill.uk>
2017-12-02 12:31:17 +0000

    Refactor command line parsing code to support --check

--- src/main.rs
+++ src/main.rs
@@ -50,56 +50,76 @@ fn list_stars() -> Vec<String> {
     names
 }
 
+/// Attempts to check all the available stars using data from the given directory (relative to the
+/// current directory)
+///
+/// This function prints its results to standard output and returns a boolean indicating if all the
+/// tests passed.
+fn check(data_dir: &str) -> bool {
+    false
+}
+
 /// Prints the usage message for this program
 fn print_usage() {
     eprintln!("aoc <star name>");
     eprintln!(" runs the given star (pass input via stdin)");
     eprintln!("aoc --list");
     eprintln!(" prints the list of available stars");
+    eprintln!("aoc --check <data dir>");
+    eprintln!(" checks all the stars using data in the given directory");
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let option_arg = if args.len() >= 2 { Some(args[1].as_ref()) } else { None };
+
     std::process::exit(
-        match args.len() {
-            0 | 1 => { print_usage(); 0 },
-            2 => {
-                match star_function(&args[1]) {
-                    Some(func) => {
-                        // Consume all of stdin and run the star!
-                        let mut stdin = String::new();
-                        match io::stdin().read_to_string(&mut stdin) {
-                            Ok(_) => {
-                                println!("{}", func(&stdin));
-                                0
-                            },
-                            Err(e) => {
-                                eprintln!("{}: read error: {}", args[0], e);
-                                1
+        match option_arg {
+            Some("--help") | Some("-h") | None => { print_usage(); 0 },
+            Some("--list") | Some("-l") => {
+                for name in list_stars() {
+                    println!("{}", name);
+                }
+                0
+            },
+            Some("--check") | Some("-c") | Some ("--test") | Some("-t") => {
+                if args.len() == 3 {
+                    if check(args[2].as_ref()) { 0 } else { 1 }
+                } else {
+                    eprintln!("run --check with one data directory argument");
+                    1
+                }
+            },
+            Some(arg) => {
+                if args.len() != 2 {
+                    eprintln!("too many arguments");
+                    1
+                } else if arg.starts_with("--") {
+                    eprintln!("unknown option \"{}\"", arg);
+                    1
+                } else {
+                    match star_function(&args[1]) {
+                        Some(func) => {
+                            // Consume all of stdin and run the star!
+                            let mut stdin = String::new();
+                            match io::stdin().read_to_string(&mut stdin) {
+                                Ok(_) => {
+                                    println!("{}", func(&stdin));
+                                    0
+                                },
+                                Err(e) => {
+                                    eprintln!("read error: {}", e);
+                                    1
+                                }
                             }
                         }
-                    }
-                    None => {
-                        // Try known arguments
-                        match args[1].as_ref() {
-                            "--help" | "-h" => { print_usage(); 0 },
-                            "--list" | "-l" => {
-                                for name in list_stars() {
-                                    println!("{}", name);
-                                }
-                                0
-                            },
-                            _ => {
-                                let thing =
-                                    if args[1].starts_with("--") { "option" } else { "star" };
-                                eprintln!("{}: unknown {} \"{}\"", args[0], thing, args[1]);
-                                1
-                            }
+                        None => {
+                            eprintln!("unknown star \"{}\"", arg);
+                            1
                         }
                     }
                 }
             },
-            _ => { eprintln!("{}: too many arguments", args[0]); 1 }
         }
     );
 }
