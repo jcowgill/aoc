3ebac9df1666ff974b8b3b88b2da7ddda9323d2f
James Cowgill <jcowgill@jcowgill.uk>
2017-12-02 18:17:13 +0000

    2017-01-1: implement star

--- src/yr2017/day1.rs
+++ src/yr2017/day1.rs
@@ -1,7 +1,24 @@
-pub fn run1(input: &str) -> String {
-    return String::from("Hello World");
+/// Sum of all digits that match the next digit in the list
+pub fn star1(input: &str) -> String {
+    if input.is_empty() {
+        return String::from("0");
+    }
+
+    let chars: Vec<char> = input.chars().collect();
+    let mut last_char = *chars.last().unwrap();
+    let mut sum = 0;
+
+    for c in chars {
+        if c == last_char {
+            sum += c.to_digit(10).unwrap();
+        }
+
+        last_char = c;
+    }
+
+    sum.to_string()
 }
 
-pub fn run2(input: &str) -> String {
+pub fn star2(input: &str) -> String {
     return String::from("Hello Worldz");
 }
--- src/yr2017/mod.rs
+++ src/yr2017/mod.rs
@@ -7,7 +7,7 @@ use StarVector;
 /// Returns the list of stars implemented this year
 pub fn stars() -> StarVector {
     vec![
-        ("01-1", day1::run1),
-        ("01-2", day1::run2),
+        ("01-1", day1::star1),
+        ("01-2", day1::star2),
     ]
 }
