4a48a144a619387dc8bd3671c1e966308481933e
James Cowgill <jcowgill@jcowgill.uk>
2017-12-02 19:33:47 +0000

    2017-02: refactor common code into separate function

--- src/yr2017/day2.rs
+++ src/yr2017/day2.rs
@@ -1,23 +1,29 @@
 use cartesian_product;
 
-/// Calculate spreadsheet checksum
-pub fn star1(input: &str) -> String {
+/// Implements a generic spreadsheet checksum function
+///  Parses the input data and passes the vector of values on each line to line_func,
+///  then sums all the lines up and returns the result
+fn spreadsheet_checksum<F>(input: &str, line_value: F) -> String where F: Fn(&Vec<i32>) -> i32 {
     let line_checksum = |line: &str| {
         let values: Vec<i32> =
             line.split_whitespace().map(|value| value.parse().unwrap()).collect();
-        values.iter().max().unwrap_or(&0) - values.iter().min().unwrap_or(&0)
+        line_value(&values)
     };
 
     let sum: i32 = input.lines().map(line_checksum).sum();
     sum.to_string()
 }
 
+/// Calculate spreadsheet checksum
+pub fn star1(input: &str) -> String {
+    spreadsheet_checksum(input, |values| {
+        values.iter().max().unwrap_or(&0) - values.iter().min().unwrap_or(&0)
+    })
+}
+
 /// Calculate spreadsheet checksum using evenly divisible numbers
 pub fn star2(input: &str) -> String {
-    let line_checksum = |line: &str| {
-        let values: Vec<i32> =
-            line.split_whitespace().map(|value| value.parse().unwrap()).collect();
-
+    spreadsheet_checksum(input, |values| {
         for (a, b) in cartesian_product(&values, &values) {
             if a != b && a % b == 0 {
                 return a / b;
@@ -25,8 +31,5 @@ pub fn star2(input: &str) -> String {
         }
 
         panic!("input line contains no valid pairs: {:?}", values)
-    };
-
-    let sum: i32 = input.lines().map(line_checksum).sum();
-    sum.to_string()
+    })
 }
