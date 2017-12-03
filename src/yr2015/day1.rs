b5c192be560b0b508a86303f81865911f32e83fe
James Cowgill <jcowgill@jcowgill.uk>
2017-12-03 14:58:44 +0000

    2015-01: implement and add tests

--- src/yr2015/day1.rs
+++ src/yr2015/day1.rs
@@ -1,7 +1,20 @@
+/// Appartment building floor using ( and ) characters
 pub fn star1(input: &str) -> String {
-    String::from("Hello World")
+    let up   = input.chars().filter(|&c| c == '(').count() as i32;
+    let down = input.chars().filter(|&c| c == ')').count() as i32;
+    (up - down).to_string()
 }
 
+/// Appartment building, when do you enter basement?
 pub fn star2(input: &str) -> String {
-    String::from("Hello World")
+    let mut floor = 0;
+    for (i, c) in input.chars().enumerate() {
+        if c == '(' { floor += 1 };
+        if c == ')' { floor -= 1 };
+        if floor < 0 {
+            return (i + 1).to_string()
+        }
+    }
+
+    panic!("never enters basement!")
 }
