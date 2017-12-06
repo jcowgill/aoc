06176972151a3a06fde23636a53a32523a0aa910
James Cowgill <jcowgill@jcowgill.uk>
2017-12-06 21:07:54 +0000

    2017-06: avoid creating peekable iterator

--- src/yr2017/day6.rs
+++ src/yr2017/day6.rs
@@ -2,22 +2,22 @@ use std::collections::HashSet;
 
 /// Returns the index of the largest element in the iterator
 ///  Returns the first index on ties
-fn first_max_index<T, Iter>(iter: Iter) -> Option<usize> where T: Ord, Iter: Iterator<Item=T> {
-    let mut peekable_iter = iter.peekable();
-    if peekable_iter.peek() != None {
-        let mut max = (peekable_iter.next().unwrap(), 0);
-        let mut index = 1;
-        for value in peekable_iter {
-            if value > max.0 {
-                max = (value, index);
+fn first_max_index<T, Iter>(mut iter: Iter) -> Option<usize> where T: Ord, Iter: Iterator<Item=T> {
+    match iter.next() {
+        Some(value) => {
+            let mut max = (value, 0);
+            let mut index = 1;
+            for value in iter {
+                if value > max.0 {
+                    max = (value, index);
+                }
+
+                index += 1;
             }
 
-            index += 1;
-        }
-
-        Some(max.1)
-    } else {
-        None
+            Some(max.1)
+        },
+        None => None
     }
 }
 
