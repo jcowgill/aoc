c3710ceaae1123ad32d377ebf26f759c130eeadf
James Cowgill <jcowgill@jcowgill.uk>
2018-11-30 23:14:34 +0000

    lib: make StarVector private
    It's arguably an implementation detail of the functions used to gather
    the full list of stars.

--- src/yr2017/mod.rs
+++ src/yr2017/mod.rs
@@ -4,8 +4,4 @@ mod processor;
 mod_all_days!();
 
 use StarVector;
-
-/// Returns the list of stars implemented this year
-pub fn stars() -> StarVector {
-    vec_all_days!()
-}
+pub fn stars() -> StarVector { vec_all_days!() }
