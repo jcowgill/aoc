1a5f67ea310d1b08dc60dedd2ca02487b0f12daf
James Cowgill <jcowgill@jcowgill.uk>
2018-12-01 10:48:12 +0000

    2018-01: implement

--- src/yr2018/day1.rs
+++ src/yr2018/day1.rs
@@ -1,7 +1,30 @@
-pub fn star1(_input: &str) -> String {
-    unimplemented!()
+use std::collections::HashSet;
+
+/// Parses the input string and returns an iterator over each integer
+fn parse_input<'a>(input: &'a str) -> impl Iterator<Item=i32> + Clone + 'a {
+    input.lines().map(|line| line.trim().parse::<i32>().unwrap())
 }
 
-pub fn star2(_input: &str) -> String {
-    unimplemented!()
+/// Calculate resulting frequency from given list of signed integers
+pub fn star1(input: &str) -> String {
+    parse_input(input).sum::<i32>().to_string()
+}
+
+/// Find the first intermediate frequency which appears twice
+pub fn star2(input: &str) -> String {
+    let mut set = HashSet::new();
+    let mut total: i32 = 0;
+
+    // Add initial zero to the set
+    set.insert(0);
+
+    // For each integer, add once to the set or return if it's already there
+    for integer in parse_input(input).cycle() {
+        total += integer;
+        if !set.insert(total) {
+            return total.to_string();
+        }
+    }
+
+    unreachable!()
 }
