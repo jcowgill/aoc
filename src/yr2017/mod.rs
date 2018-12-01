10d5cc8fb810b218997cba7d2147e34b3788cd92
James Cowgill <jcowgill@jcowgill.uk>
2018-12-01 00:42:10 +0000

    lib: use new StarId instead of str to identify stars
    Switch to a new StarId structure to identify stars instead of using
    strings. This makes it easier for different parts of code to
    understand the hierarchy instead of treating star ids as opaque
    strings.
    
    In addition, restructure the code so that there is never any star map
    nesting.

--- src/yr2017/mod.rs
+++ src/yr2017/mod.rs
@@ -3,5 +3,5 @@
 mod processor;
 mod_all_days!();
 
-use StarVector;
-pub fn stars() -> StarVector { vec_all_days!() }
+use StarFunction; use StarId;
+pub fn stars() -> Vec<(StarId, StarFunction)> { vec_all_days!(2017) }
