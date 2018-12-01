05253282790177a45e0c5cec0180937304dee5aa
James Cowgill <jcowgill@jcowgill.uk>
2018-12-01 10:17:05 +0000

    tests: add leading 0 to first 10 days
    Star names always have a leading 0 in day names, however the test
    system was searching for stars without a leading 0. Fix this so that
    the tests run again.

--- tests/main.rs
+++ tests/main.rs
@@ -103,7 +103,7 @@ macro_rules! gen_tests {
         mod $year {
             use super::gen_tests_helper;
             gen_tests_days!($year,
-                            day1, day2, day3, day4, day5, day6, day7, day8, day9,
+                            day01, day02, day03, day04, day05, day06, day07, day08, day09,
                             day10, day11, day12, day13, day14, day15, day16, day17,
                             day18, day19, day20, day21, day22, day23, day24, day25);
         }
