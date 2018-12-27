cd44ed73adad281f6d6a92fa35f43b98536d7fc2
James Cowgill <jcowgill@jcowgill.uk>
2018-12-27 23:29:58 +0000

    2018-05: implement

--- src/yr2018/day5.rs
+++ src/yr2018/day5.rs
@@ -1,7 +1,33 @@
-pub fn star1(_input: &str) -> String {
-    unimplemented!()
+/// Predicate which returns true if two units react
+fn units_react(a: char, b: char) -> bool {
+    a != b && a.to_ascii_lowercase() == b.to_ascii_lowercase()
 }
 
-pub fn star2(_input: &str) -> String {
-    unimplemented!()
+/// React a polymer returning a new vector
+fn react<I: Iterator<Item=char>>(polymer: I) -> Vec<char> {
+    let mut result = Vec::new();
+
+    for c in polymer {
+        assert!(c.is_ascii_alphabetic());
+
+        // React with most recent character, or append to the result
+        if !result.is_empty() && units_react(c, result[result.len() - 1]) {
+            result.pop();
+        } else {
+            result.push(c);
+        }
+    }
+
+    result
+}
+
+pub fn star1(input: &str) -> String {
+    react(input.chars()).len().to_string()
+}
+
+pub fn star2(input: &str) -> String {
+    (b'a'..=b'z')
+        .map(|r| char::from(r))
+        .map(|r| react(input.chars().filter(|c| c.to_ascii_lowercase() != r)).len())
+        .min().unwrap().to_string()
 }
