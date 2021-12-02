be9d3b7f43947f867b55e24cdc8c04bce95784ee
James Cowgill <jcowgill@jcowgill.uk>
2021-12-02 21:19:26 +0000

    2021-02: implement

--- src/yr2021/day2.rs
+++ src/yr2021/day2.rs
@@ -1,7 +1,63 @@
-pub fn star1(_input: &str) -> String {
-    unimplemented!()
+use std::str::FromStr;
+
+use strum::EnumString;
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq, EnumString)]
+#[strum(serialize_all = "lowercase")]
+enum CommandName {
+    Forward,
+    Down,
+    Up,
 }
 
-pub fn star2(_input: &str) -> String {
-    unimplemented!()
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+struct Command {
+    name: CommandName,
+    amount: i32,
+}
+
+impl FromStr for Command {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (cmd_str, amount_str) = s.split_once(' ').ok_or(())?;
+        Ok(Command {
+            name: cmd_str.parse().map_err(|_| ())?,
+            amount: amount_str.parse().map_err(|_| ())?,
+        })
+    }
+}
+
+pub fn star1(input: &str) -> String {
+    let mut fwd = 0;
+    let mut depth = 0;
+
+    for cmd in input.lines().map(|l| l.parse::<Command>().unwrap()) {
+        match cmd.name {
+            CommandName::Forward => fwd += cmd.amount,
+            CommandName::Down => depth += cmd.amount,
+            CommandName::Up => depth -= cmd.amount,
+        }
+    }
+
+    (fwd * depth).to_string()
+}
+
+pub fn star2(input: &str) -> String {
+    let mut fwd = 0;
+    let mut depth = 0;
+    let mut aim = 0;
+
+    for cmd in input.lines().map(|l| l.parse::<Command>().unwrap()) {
+        match cmd.name {
+            CommandName::Forward => {
+                fwd += cmd.amount;
+                depth += aim * cmd.amount;
+            }
+            CommandName::Down => aim += cmd.amount,
+            CommandName::Up => aim -= cmd.amount,
+        }
+    }
+
+    (fwd * depth).to_string()
 }
