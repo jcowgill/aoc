d34d4b910c32a30e568fb1915f4d48e2e226168a
James Cowgill <jcowgill@jcowgill.uk>
2021-12-07 08:23:09 +0000

    2021-07: implement

--- src/yr2021/day7.rs
+++ src/yr2021/day7.rs
@@ -1,7 +1,34 @@
-pub fn star1(_input: &str) -> String {
-    unimplemented!()
+pub fn star1(input: &str) -> String {
+    let mut positions: Vec<i32> = input
+        .split(',')
+        .map(|n| n.trim().parse().unwrap())
+        .collect();
+    positions.sort_unstable();
+    let median = positions[positions.len() / 2];
+    positions
+        .iter()
+        .map(|p| (p - median).abs())
+        .sum::<i32>()
+        .to_string()
 }
 
-pub fn star2(_input: &str) -> String {
-    unimplemented!()
+pub fn star2(input: &str) -> String {
+    let positions: Vec<i32> = input
+        .split(',')
+        .map(|n| n.trim().parse().unwrap())
+        .collect();
+    let mean1 = positions.iter().sum::<i32>() / (positions.len() as i32);
+
+    // Try mean and mean + 1 as the midpoint in case the mean is not an integer
+    (mean1..=mean1 + 1)
+        .map(|mean| {
+            positions
+                .iter()
+                .map(|p| (p - mean).abs())
+                .map(|r| (r * (r + 1)) / 2)
+                .sum::<i32>()
+        })
+        .min()
+        .unwrap()
+        .to_string()
 }
