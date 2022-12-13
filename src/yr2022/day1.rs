18379d80e757dc32b98663ee396c1570864e3de1
James Cowgill <jcowgill@jcowgill.uk>
2022-12-13 21:08:39 +0000

    2022-01: implement

--- src/yr2022/day1.rs
+++ src/yr2022/day1.rs
@@ -1,7 +1,31 @@
-pub fn star1(_input: &str) -> String {
-    unimplemented!()
+fn add_value<const N: usize>(max: &mut [u32; N], value: u32) {
+    let min = max.iter_mut().min().unwrap();
+    if *min < value {
+        *min = value;
+    }
 }
 
-pub fn star2(_input: &str) -> String {
-    unimplemented!()
+fn top_k<const N: usize>(input: &str) -> String {
+    let mut max = [0; N];
+    let mut current = 0;
+
+    for line in input.lines().map(|l| l.trim()) {
+        if line.is_empty() {
+            add_value(&mut max, current);
+            current = 0;
+        } else {
+            current += line.parse::<u32>().unwrap();
+        }
+    }
+
+    add_value(&mut max, current);
+    max.into_iter().sum::<u32>().to_string()
+}
+
+pub fn star1(input: &str) -> String {
+    top_k::<1>(input)
+}
+
+pub fn star2(input: &str) -> String {
+    top_k::<3>(input)
 }
