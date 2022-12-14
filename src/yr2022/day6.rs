4313abfb1b2d34f15e46989d6b85dde1d47dc97f
James Cowgill <jcowgill@jcowgill.uk>
2022-12-14 00:17:58 +0000

    2022-06: implement

--- src/yr2022/day6.rs
+++ src/yr2022/day6.rs
@@ -1,7 +1,27 @@
-pub fn star1(_input: &str) -> String {
-    unimplemented!()
+fn star(input: &str, n: usize) -> String {
+    // Invariant: all characters in buffer are unique
+    let mut buffer = Vec::with_capacity(n);
+
+    for (i, c) in input.chars().enumerate() {
+        // Is this character unique?
+        if let Some(pos) = buffer.iter().position(|&b| b == c) {
+            buffer.drain(..=pos);
+        }
+
+        buffer.push(c);
+
+        if buffer.len() >= n {
+            return (i + 1).to_string();
+        }
+    }
+
+    panic!("could not find sequence");
 }
 
-pub fn star2(_input: &str) -> String {
-    unimplemented!()
+pub fn star1(input: &str) -> String {
+    star(input, 4)
+}
+
+pub fn star2(input: &str) -> String {
+    star(input, 14)
 }
