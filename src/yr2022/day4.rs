907ab78b98d3bcced3925af8cc638ad2f95216a5
James Cowgill <jcowgill@jcowgill.uk>
2022-12-14 00:17:39 +0000

    2022-04: implement

--- src/yr2022/day4.rs
+++ src/yr2022/day4.rs
@@ -1,7 +1,29 @@
-pub fn star1(_input: &str) -> String {
-    unimplemented!()
+use itertools::Itertools;
+
+fn parse_input(input: &str) -> impl Iterator<Item = (i32, i32, i32, i32)> + '_ {
+    input.lines().map(|line| {
+        line.trim()
+            .split(['-', ','])
+            .map(|v| v.parse::<i32>().unwrap())
+            .collect_tuple()
+            .unwrap()
+    })
 }
 
-pub fn star2(_input: &str) -> String {
-    unimplemented!()
+pub fn star1(input: &str) -> String {
+    parse_input(input)
+        .filter(|(a1, a2, b1, b2)| {
+            let c1 = a1.max(b1);
+            let c2 = a2.min(b2);
+            (c1 == a1 && c2 == a2) || (c1 == b1 && c2 == b2)
+        })
+        .count()
+        .to_string()
+}
+
+pub fn star2(input: &str) -> String {
+    parse_input(input)
+        .filter(|(a1, a2, b1, b2)| a1.max(b1) <= a2.min(b2))
+        .count()
+        .to_string()
 }
