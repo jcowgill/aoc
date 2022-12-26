d8994753a9aa4b7b0b8297384fdf6105a2cb960e
James Cowgill <jcowgill@jcowgill.uk>
2022-12-26 00:14:15 +0000

    2022-25: implement

--- src/yr2022/day25.rs
+++ src/yr2022/day25.rs
@@ -1,3 +1,29 @@
-pub fn star1(_input: &str) -> String {
-    unimplemented!()
+fn from_snafu(num: &str) -> i64 {
+    num.chars().fold(0, |acc, c| {
+        acc * 5
+            + match c {
+                '2' => 2,
+                '1' => 1,
+                '0' => 0,
+                '-' => -1,
+                '=' => -2,
+                _ => panic!("invalid character {}", c),
+            }
+    })
+}
+
+fn to_snafu(num: i64) -> String {
+    match num % 5 {
+        0 if num == 0 => String::new(),
+        0 => to_snafu(num / 5) + "0",
+        1 => to_snafu(num / 5) + "1",
+        2 => to_snafu(num / 5) + "2",
+        3 => to_snafu(num / 5 + 1) + "=",
+        4 => to_snafu(num / 5 + 1) + "-",
+        _ => panic!("invalid number"),
+    }
+}
+
+pub fn star1(input: &str) -> String {
+    to_snafu(input.lines().map(from_snafu).sum::<i64>())
 }
