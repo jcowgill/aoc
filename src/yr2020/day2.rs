e91cc46fa9d604d73b32277cf4ee66243a2f4e1f
James Cowgill <jcowgill@jcowgill.uk>
2023-07-02 23:38:11 +0100

    2020-02: implement

--- src/yr2020/day2.rs
+++ src/yr2020/day2.rs
@@ -1,7 +1,34 @@
-pub fn star1(_input: &str) -> String {
-    todo!()
+fn parse_line(line: &str) -> (usize, usize, char, &str) {
+    let parts: Vec<_> = line.split(&[' ', ':', '-']).collect();
+    (
+        parts[0].parse().unwrap(),
+        parts[1].parse().unwrap(),
+        parts[2].chars().next().unwrap(),
+        parts[4],
+    )
 }
 
-pub fn star2(_input: &str) -> String {
-    todo!()
+pub fn star1(input: &str) -> String {
+    input
+        .lines()
+        .filter(|&line| {
+            let (min, max, ch, s) = parse_line(line);
+            let count = s.chars().filter(|&c| c == ch).count();
+            (min..=max).contains(&count)
+        })
+        .count()
+        .to_string()
+}
+
+pub fn star2(input: &str) -> String {
+    input
+        .lines()
+        .filter(|&line| {
+            let (a, b, ch, s) = parse_line(line);
+            let count = (s.chars().nth(a - 1) == Some(ch)) as i32
+                + (s.chars().nth(b - 1) == Some(ch)) as i32;
+            count == 1
+        })
+        .count()
+        .to_string()
 }
