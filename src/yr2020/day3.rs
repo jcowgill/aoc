3cf66af08c64793a0cc725f47251f8f35e063ff7
James Cowgill <jcowgill@jcowgill.uk>
2023-07-03 00:36:34 +0100

    2020-03: implement

--- src/yr2020/day3.rs
+++ src/yr2020/day3.rs
@@ -1,7 +1,37 @@
-pub fn star1(_input: &str) -> String {
-    todo!()
+use nalgebra::DMatrix;
+
+fn parse_grid(input: &str) -> DMatrix<bool> {
+    let width = input.lines().next().unwrap().len();
+    let height = input.lines().count();
+    DMatrix::from_row_iterator(
+        height,
+        width,
+        input
+            .lines()
+            .flat_map(|line| line.chars().map(|c| c == '#')),
+    )
 }
 
-pub fn star2(_input: &str) -> String {
-    todo!()
+fn count_trees(grid: &DMatrix<bool>, dx: usize, dy: usize) -> usize {
+    (0..grid.nrows())
+        .step_by(dy)
+        .filter(|&y| {
+            let x = (y * dx / dy) % grid.ncols();
+            grid[(y, x)]
+        })
+        .count()
+}
+
+pub fn star1(input: &str) -> String {
+    count_trees(&parse_grid(input), 3, 1).to_string()
+}
+
+pub fn star2(input: &str) -> String {
+    let grid = parse_grid(input);
+    (count_trees(&grid, 1, 1)
+        * count_trees(&grid, 3, 1)
+        * count_trees(&grid, 5, 1)
+        * count_trees(&grid, 7, 1)
+        * count_trees(&grid, 1, 2))
+    .to_string()
 }
