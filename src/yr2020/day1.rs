ae6783dfb2f72105803a004eaf783cfb51b18ead
James Cowgill <jcowgill@jcowgill.uk>
2023-07-02 23:03:39 +0100

    2020-01: implement

--- src/yr2020/day1.rs
+++ src/yr2020/day1.rs
@@ -1,7 +1,22 @@
-pub fn star1(_input: &str) -> String {
-    todo!()
+use std::collections::HashSet;
+
+fn parse_input(input: &str) -> HashSet<i32> {
+    input.lines().map(|l| l.parse().unwrap()).collect()
 }
 
-pub fn star2(_input: &str) -> String {
-    todo!()
+pub fn star1(input: &str) -> String {
+    let nums = parse_input(input);
+    let a = *nums.iter().find(|&&n| nums.contains(&(2020 - n))).unwrap();
+    (a * (2020 - a)).to_string()
+}
+
+pub fn star2(input: &str) -> String {
+    let nums = parse_input(input);
+    for &a in nums.iter() {
+        if let Some(&b) = nums.iter().find(|&&n| nums.contains(&(2020 - a - n))) {
+            return (a * b * (2020 - a - b)).to_string();
+        }
+    }
+
+    panic!("invalid input")
 }
