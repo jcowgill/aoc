1ef4999e47f6c708e299b8600c0b5d6f7c45c8ff
James Cowgill <jcowgill@jcowgill.uk>
2023-07-18 19:58:59 +0100

    2020-06: implement

--- src/yr2020/day6.rs
+++ src/yr2020/day6.rs
@@ -1,7 +1,27 @@
-pub fn star1(_input: &str) -> String {
-    todo!()
+pub fn star1(input: &str) -> String {
+    input
+        .split("\n\n")
+        .map(|group| {
+            let mut ans: Vec<char> = group.chars().filter(char::is_ascii_alphabetic).collect();
+            ans.sort_unstable();
+            ans.dedup();
+            ans.len()
+        })
+        .sum::<usize>()
+        .to_string()
 }
 
-pub fn star2(_input: &str) -> String {
-    todo!()
+pub fn star2(input: &str) -> String {
+    input
+        .split("\n\n")
+        .map(|group| {
+            let mut lines = group.lines();
+            let mut all: Vec<char> = lines.next().unwrap().chars().collect();
+            for l in lines {
+                all.retain(|&c| l.chars().any(|c2| c == c2))
+            }
+            all.len()
+        })
+        .sum::<usize>()
+        .to_string()
 }
