c8ce63d7e9e8cfe2305f4fc208c778ec8fe048ea
James Cowgill <jcowgill@jcowgill.uk>
2023-07-18 19:58:52 +0100

    2020-05: implement

--- src/yr2020/day5.rs
+++ src/yr2020/day5.rs
@@ -1,7 +1,24 @@
-pub fn star1(_input: &str) -> String {
-    todo!()
+fn seats(input: &str) -> impl Iterator<Item = u16> + '_ {
+    input.lines().map(|l| {
+        l.chars()
+            .fold(0, |acc, c| acc << 1 | (c == 'B' || c == 'R') as u16)
+    })
 }
 
-pub fn star2(_input: &str) -> String {
-    todo!()
+pub fn star1(input: &str) -> String {
+    seats(input).max().unwrap().to_string()
+}
+
+pub fn star2(input: &str) -> String {
+    let mut v: Vec<u16> = seats(input).collect();
+    v.sort_unstable();
+    let min = (*v.first().unwrap() | 7) + 1;
+    let max = *v.last().unwrap() & !7;
+    v.into_iter()
+        .filter(|id| (min..max).contains(id))
+        .zip(min..max)
+        .find(|(a, b)| a != b)
+        .unwrap()
+        .1
+        .to_string()
 }
