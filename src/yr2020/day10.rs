0e8af31fe2756d37d363f3531d4d7c98b9099c19
James Cowgill <jcowgill@jcowgill.uk>
2023-07-23 19:24:27 +0100

    2020-10: implement

--- src/yr2020/day10.rs
+++ src/yr2020/day10.rs
@@ -1,7 +1,34 @@
-pub fn star1(_input: &str) -> String {
-    todo!()
+fn parse_input(input: &str) -> Vec<u32> {
+    let mut nums: Vec<u32> = input.lines().map(|l| l.parse().unwrap()).collect();
+    nums.push(0);
+    nums.sort_unstable();
+    nums.push(nums.last().unwrap() + 3);
+    nums
 }
 
-pub fn star2(_input: &str) -> String {
-    todo!()
+pub fn star1(input: &str) -> String {
+    let nums = parse_input(input);
+    let mut jumps = [0, 0, 0];
+    for w in nums.windows(2) {
+        jumps[(w[1] - w[0] - 1) as usize] += 1;
+    }
+
+    (jumps[0] * jumps[2]).to_string()
+}
+
+pub fn star2(input: &str) -> String {
+    let nums = parse_input(input);
+    let mut arrangements = vec![1u64; nums.len()];
+
+    for (i, n1) in nums[..nums.len() - 2].iter().enumerate().rev() {
+        arrangements[i] = arrangements[i + 1];
+        if nums[i + 2] - n1 <= 3 {
+            arrangements[i] += arrangements[i + 2];
+        }
+        if i + 3 < nums.len() && nums[i + 3] - n1 <= 3 {
+            arrangements[i] += arrangements[i + 3];
+        }
+    }
+
+    arrangements[0].to_string()
 }
