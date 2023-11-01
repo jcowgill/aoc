7b2a044b149c073cd4a0b95c6629902a18aa13d7
James Cowgill <jcowgill@jcowgill.uk>
2023-11-01 01:18:49 +0000

    2020-25: implement

--- src/yr2020/day25.rs
+++ src/yr2020/day25.rs
@@ -1,3 +1,21 @@
-pub fn star1(_input: &str) -> String {
-    todo!()
+const MODULUS: u64 = 20_201_227;
+
+fn pow_modulo(base: u64, exponent: u64) -> u64 {
+    (0..exponent).fold(1, |acc, _| (acc * base) % MODULUS)
+}
+
+fn discrete_log(base: u64, result: u64) -> u64 {
+    let mut prev = 1;
+    for n in 0..MODULUS {
+        if prev == result {
+            return n;
+        }
+        prev = (prev * base) % MODULUS;
+    }
+    panic!("no solution");
+}
+
+pub fn star1(input: &str) -> String {
+    let keys: Vec<u64> = input.lines().map(|l| l.parse().unwrap()).collect();
+    pow_modulo(keys[0], discrete_log(7, keys[1])).to_string()
 }
