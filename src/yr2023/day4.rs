c02621af171bb6039b3b087d6b550f67490d4cd0
James Cowgill <jcowgill@jcowgill.uk>
2023-12-04 09:16:40 +0000

    2023-04: implement

--- src/yr2023/day4.rs
+++ src/yr2023/day4.rs
@@ -1,7 +1,37 @@
-pub fn star1(_input: &str) -> String {
-    todo!()
+use std::collections::HashSet;
+
+fn parse_nums(nums: &str) -> HashSet<u32> {
+    nums.split_ascii_whitespace()
+        .map(|n| n.parse().unwrap())
+        .collect()
 }
 
-pub fn star2(_input: &str) -> String {
-    todo!()
+fn winning_cards(input: &str) -> impl Iterator<Item = usize> + '_ {
+    input.lines().map(|line| {
+        let (l_str, r_str) = line.split_once(':').unwrap().1.split_once('|').unwrap();
+        parse_nums(l_str).intersection(&parse_nums(r_str)).count()
+    })
+}
+
+pub fn star1(input: &str) -> String {
+    winning_cards(input)
+        .map(|w| if w > 0 { 1 << (w - 1) } else { 0 })
+        .sum::<usize>()
+        .to_string()
+}
+
+pub fn star2(input: &str) -> String {
+    let winnings: Vec<_> = winning_cards(input).collect();
+    let mut cards = vec![1; winnings.len()];
+
+    for i in 0..cards.len() {
+        let card_num = cards[i];
+        for j in 0..winnings[i] {
+            if let Some(n) = cards.get_mut(i + j + 1) {
+                *n += card_num;
+            }
+        }
+    }
+
+    cards.into_iter().sum::<u32>().to_string()
 }
