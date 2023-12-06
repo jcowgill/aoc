c28220b09c9502971514a1be2940e2cba5c3064e
James Cowgill <jcowgill@jcowgill.uk>
2023-12-06 22:43:28 +0000

    2023-06: implement

--- src/yr2023/day6.rs
+++ src/yr2023/day6.rs
@@ -1,7 +1,47 @@
-pub fn star1(_input: &str) -> String {
-    todo!()
+use itertools::{multizip, Itertools};
+
+fn solve((t, d): (i64, i64)) -> i64 {
+    // y = x * (t - x) - d = xt - x^2 - (d + 1)
+    // Count number of integer x such that y >= 0
+    let sqr_d2 = ((t * t - 4 * (d + 1)) as f64).sqrt() / 2.0;
+    let min = (t as f64 / 2.0 - sqr_d2).ceil() as i64;
+    let max = (t as f64 / 2.0 + sqr_d2).floor() as i64;
+    max - min + 1
 }
 
-pub fn star2(_input: &str) -> String {
-    todo!()
+pub fn star1(input: &str) -> String {
+    multizip(
+        input
+            .lines()
+            .map(|line| {
+                line.split_ascii_whitespace()
+                    .skip(1)
+                    .map(|n| n.parse::<i64>().unwrap())
+            })
+            .collect_tuple::<(_, _)>()
+            .unwrap(),
+    )
+    .map(solve)
+    .product::<i64>()
+    .to_string()
+}
+
+pub fn star2(input: &str) -> String {
+    solve(
+        input
+            .lines()
+            .map(|line| {
+                line.split_once(':')
+                    .unwrap()
+                    .1
+                    .chars()
+                    .filter(char::is_ascii_digit)
+                    .collect::<String>()
+                    .parse::<i64>()
+                    .unwrap()
+            })
+            .collect_tuple()
+            .unwrap(),
+    )
+    .to_string()
 }
