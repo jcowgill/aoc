c0b96a89f3d1c2a8e07b1519c40977fe5c14b389
James Cowgill <jcowgill@jcowgill.uk>
2023-12-09 19:09:14 +0000

    2023-09: implement

--- src/yr2023/day9.rs
+++ src/yr2023/day9.rs
@@ -1,7 +1,33 @@
-pub fn star1(_input: &str) -> String {
-    todo!()
+fn solve(input: &str, rev: bool) -> String {
+    input
+        .lines()
+        .map(|line| {
+            let mut nums: Vec<_> = line
+                .split_ascii_whitespace()
+                .map(|n| n.parse::<i32>().unwrap())
+                .collect();
+            if rev {
+                nums.reverse();
+            }
+
+            (1..nums.len())
+                .scan(nums, |state, _| {
+                    Some(std::mem::replace(
+                        state,
+                        state.windows(2).map(|w| w[1] - w[0]).collect(),
+                    ))
+                })
+                .map(|d| *d.last().unwrap())
+                .sum::<i32>()
+        })
+        .sum::<i32>()
+        .to_string()
 }
 
-pub fn star2(_input: &str) -> String {
-    todo!()
+pub fn star1(input: &str) -> String {
+    solve(input, false)
+}
+
+pub fn star2(input: &str) -> String {
+    solve(input, true)
 }
