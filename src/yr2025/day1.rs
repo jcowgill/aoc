d77b26eb7f60084ae4b8056306594a0cccb211a3
James Cowgill <jcowgill@jcowgill.uk>
2025-12-22 22:41:56 +0000

    2025-01: implement

--- src/yr2025/day1.rs
+++ src/yr2025/day1.rs
@@ -1,7 +1,52 @@
-pub fn star1(_input: &str) -> String {
-    todo!()
+fn parse(input: &str) -> impl Iterator<Item = i32> + '_ {
+    input
+        .lines()
+        .map(|l| {
+            let mut chars = l.chars();
+            let c1 = chars.next().expect("invald direction");
+            let n: i32 = chars.as_str().parse().expect("invalid number");
+            match c1 {
+                'R' => n,
+                'L' => -n,
+                _ => panic!("invalid direction"),
+            }
+        })
+        .filter(|&i| i != 0)
 }
 
-pub fn star2(_input: &str) -> String {
-    todo!()
+pub fn star1(input: &str) -> String {
+    let mut zeros = 0;
+    let mut acc = 50;
+
+    for v in parse(input) {
+        acc = (acc + v).rem_euclid(100);
+        if acc == 0 {
+            zeros += 1;
+        }
+    }
+
+    zeros.to_string()
+}
+
+pub fn star2(input: &str) -> String {
+    let mut zeros = 0;
+    let mut acc = 50;
+
+    for v in parse(input) {
+        let next = acc + v;
+
+        if next == 0 {
+            zeros += 1;
+        } else if next > 0 {
+            zeros += next.div_euclid(100);
+        } else if acc == 0 {
+            zeros -= next / 100;
+        } else {
+            zeros += 1 - next / 100;
+        }
+
+        acc = next.rem_euclid(100);
+    }
+
+    zeros.to_string()
 }
